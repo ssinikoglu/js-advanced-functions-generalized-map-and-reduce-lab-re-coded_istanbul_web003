c005ee1ddc5bceb73f6bad5c9a872b5af9033e1a
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-10-22 20:00:25 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+
+function map(array, func) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(func(array[i]));
+  }
+  return newArray;
+}
+
+function reduce(array, func, starting) {
+  let result;
+  if (starting) {
+    result = starting;
+    for (let i = 0; i < array.length; i++) {
+      result = func(array[i], result);
+    }
+  } else {
+    result = array[0];
+    for (let i = 1; i < array.length; i++) {
+      result = func(array[i], result);
+    }
+  }
+
+  return result;
+}
